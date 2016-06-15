35308c9e4b6b7793978d251ca7089ad4daf75931
Derek Graham <derekgraham@mac.com>
2016-06-14 23:34:43 -0700

    broken git

---
+++ derekg-week2/derekg.week2/derekg.week2/user.swift
@@ -0,0 +1,29 @@
+//
+//  user.swift
+//  derekg.week2
+//
+//  Created by Derek Graham on 6/13/16.
+//  Copyright © 2016 Derek Graham. All rights reserved.
+//
+
+import Foundation
+
+class User
+{
+    let name : String
+    let profileImageUrl : String
+    let location : String
+    
+    init?( json : [String : AnyObject])
+    {
+        if let name = json["name"] as? String,
+            profileImageUrl = json["profile_image_url"] as? String,
+            location = json["location"] as? String
+        {
+            self.name = name
+            self.profileImageUrl = profileImageUrl
+            self.location = location
+        } else { return nil }
+        
+    }
+}
